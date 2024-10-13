036dcf965fbd1f4cc2777c6d7176aae88d8d72cc
RamaRamdani03 <ramaramdani449@gamil.com>
2024-10-13 20:21:23 +0700

    Menambahkan perbandingan menggunakan fungsi equals

--- app/src/main/java/com/example/noactivity/DataClass.kt
+++ app/src/main/java/com/example/noactivity/DataClass.kt
@@ -18,3 +18,7 @@ data class DataClass(
 fun main() {
     val data = DataClass(1, "indra@gmail.com")
     println(data)
+
+    // Fungsi Equals
+    val data2 = DataClass(1, "makan@gmail.com")
+    println(data == data2) // false
