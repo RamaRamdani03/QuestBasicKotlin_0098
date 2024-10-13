a7039ac9931da0cca36edf5000c263a1062e878c
RamaRamdani03 <ramaramdani449@gamil.com>
2024-10-13 20:37:17 +0700

    Menambahkan fungsi main untuk menguji uppercaseString dan fungsi lambda dengan memanggil uppercaseString("hello") (hasil tidak dicetak) dan mencetak "hello" dalam huruf kapital menggunakan fungsi lambda, disertai komentar yang menunjukkan hasil yang diharapkan: "HELLO".

--- app/src/main/java/com/example/noactivity/LambdaFunction.kt
+++ app/src/main/java/com/example/noactivity/LambdaFunction.kt
@@ -8,3 +8,10 @@ package com.example.noactivity
 fun uppercaseString(string: String): String {
     return string.uppercase()
 }
+
+//Dapat ditulis dalam ekspresi lambda sbb :
+fun main() {
+    uppercaseString("hello")
+    println({ string: String -> string.uppercase() }("hello"))
+    //HELLO
+}
