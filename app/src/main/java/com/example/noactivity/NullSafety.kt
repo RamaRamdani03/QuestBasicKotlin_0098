da49d1bf3a5dfac98d4e485e31c2f0e845db8626
RamaRamdani03 <ramaramdani449@gamil.com>
2024-10-13 20:47:13 +0700

    Variabel `neverNull` dideklarasikan sebagai `String`, sehingga tidak dapat bernilai null dan menyebabkan kesalahan kompilasi jika diisi dengan null, sedangkan variabel `nullable` dideklarasikan sebagai `String?`, yang memungkinkan penugasan nilai null, dan dalam pemeriksaan null, blok `if` untuk `neverNull` tidak akan pernah dieksekusi karena variabel ini tidak bisa null, sehingga hanya blok `else` yang mencetak "inferredNonNull is not null".

---
+++ app/src/main/java/com/example/noactivity/NullSafety.kt
@@ -0,0 +1,23 @@
+package com.example.noactivity
+
+import org.jetbrains.annotations.NotNull
+
+fun nullSafety() {
+    // neverNull has String type
+    var neverNull: String = "This can't be null"
+
+    // Throws a compiler error
+    // neverNull = null
+
+    // nullable has nullable String type
+    var nullable: String? = "You can keep a null here"
+    //This is Ok
+    nullable = null
+
+
+    // Check for null in conditions
+    if (neverNull == null) {
+        println("inferredNonNull is null")
+    } else {
+        println("inferredNonNull is not null")
+    }
