0ded4fd00c48e48246b62422fdb86100348fd4c1
RamaRamdani03 <ramaramdani449@gamil.com>
2024-10-13 21:45:28 +0700

    fungsi ContohList mendemonstrasikan penggunaan List di Kotlin, termasuk List Read-Only yang tidak dapat dimodifikasi dan List Mutable yang dapat ditambahkan, dihapus, dan dimodifikasi; kode ini mencetak hasil dari setiap operasi, termasuk penambahan elemen baru, perubahan elemen yang ada, serta pembuatan list read-only dari list mutable yang telah dimodifikasi.

---
+++ app/src/main/java/com/example/noactivity/Collections.kt
@@ -0,0 +1,34 @@
+package com.example.noactivity
+
+// List adalah kumpulan data yang disusun secara berurutan
+// List bersifat ordered, artinya data yang dimasukkan akan memiliki posisi sesuai urutan
+// List bersifat mutable dan read only, artinya data yang dimasukkan dapat diubah dan tidak dapat diubah
+// List Read-Only menggunakan listOf
+// List Mutable menggunakan mutableListOf
+
+fun ContohList() {
+    println("=== List ===")
+    // List Read-Only
+    val readOnlyAbjad = listOf("A", "B", "C")
+    println(readOnlyAbjad)
+
+    // List Mutable
+    val shape: MutableList<String> = mutableListOf("Circle", "Square", "Triangle")
+    println(shape)
+
+    // Menambahkan data ke dalam List Mutable
+    shape.add("Circle")
+    println(shape)
+
+    // Menghapus data dari List Mutable
+    shape.add("Circle")
+    println(shape)
+
+    // Mengubah data di dalam List Mutable
+    shape[0] = "Oval"
+    println(shape)
+
+    // List Read-Only
+    val shapesLocked: List<String> = shape
+    println(shapesLocked)
+}
