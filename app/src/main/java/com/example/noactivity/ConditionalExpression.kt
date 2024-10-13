10a7baf1b49f31feaeabc1502f14635bfe280c99
RamaRamdani03 <ramaramdani449@gamil.com>
2024-10-13 21:06:56 +0700

    mendemonstrasikan berbagai jenis perulangan di Kotlin, termasuk perulangan for untuk mencetak tipe mobil dari daftar cars, perulangan while yang mencetak iterasi dari 1 hingga 5, dan perulangan do-while yang juga mencetak iterasi dari 1 hingga 5, dengan menambahkan nilai iterasi di setiap langkah.

--- app/src/main/java/com/example/noactivity/ConditionalExpression.kt
+++ app/src/main/java/com/example/noactivity/ConditionalExpression.kt
@@ -44,3 +44,21 @@ fun Perulangan() {
     for (i in 1..5) {
         println("Perulangan ke-$i")
     }
+
+    val cars = listOf("BMW", "Toyota", "Honda", "Audi")
+    for( car in cars) {
+        println("Car type: $car")
+    }
+
+    var x = 1
+    while (x <= 5) {
+        println("Perulangan ke-$x")
+        x++
+    }
+
+    var y = 1
+    do {
+        println("Perulangan ke-$y")
+        y++
+    } while (y <= 5)
+}
